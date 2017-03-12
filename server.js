e2b6a02778045d15544e47a8129f669c0ab0b87f
Uzi Harband <uharband@cisco.com>
2017-03-12 08:00:50 +0200

    added long call

--- server.js
+++ server.js
@@ -9,7 +9,16 @@ app.get('/ping', function (req, res) {
   res.send('ok')
 })
 
+app.get('/long/:duration', function (req, res) {
+  var sleepDuration = req.params.duration;
+  console.log('about to sleep for ' + sleepDuration);
+  setTimeout(function(){
+      console.log('done sleeping for ' + sleepDuration);
+      res.send('ok. slept for ' + sleepDuration + ' ms');
+  },sleepDuration);
+})
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080! try my / api!')
+
+app.listen(8090, function () {
+  console.log('Example app listening on port 8090! try my / api!')
 })
