abdaea0f583496b83a97d36c1453ecdb195c3f5f
Uzi Harband <uharband@cisco.com>
2017-07-18 12:28:54 +0300

    added payload api

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # hello-node
 basic node app
 run: node server.js
+
 apis:
 / returns hello world
 /ping returns ok
+
+delay the response for :duration milliseconds
+/long/:duration
+
+return a payload with size :bytes. the base response size is 200b. "payload" is added to that
+/payload/:bytes
--- server.js
+++ server.js
@@ -1,22 +1,30 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World! try my /ping api!')
-})
+});
 
 app.get('/ping', function (req, res) {
-  res.send('ok')
-})
+    var time = process.uptime();
+    res.send('ok. seconds up: ' + time.toString());
+});
 
 app.get('/long/:duration', function (req, res) {
-  var sleepDuration = req.params.duration;
-  console.log('about to sleep for ' + sleepDuration);
-  setTimeout(function(){
-      console.log('done sleeping for ' + sleepDuration);
-      res.send('ok. slept for ' + sleepDuration + ' ms');
-  },sleepDuration);
-})
+    var sleepDuration = req.params.duration;
+    console.log('about to sleep for ' + sleepDuration);
+    setTimeout(function(){
+        console.log('done sleeping for ' + sleepDuration);
+        res.send('ok. slept for ' + sleepDuration + ' ms');
+    },sleepDuration);
+});
+
+app.get('/payload/:bytes', function(req, res){
+    var bytes = parseInt(req.params.bytes);
+    const buf = Buffer.alloc(bytes, 'a');
+    res.send(buf.toString());
+
+});
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 
